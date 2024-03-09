04c29793e853b08209a7a9a3e92d4a2935f81f42
Inialpha <inimfonebong001@gmail.com>
2024-03-09 02:21:23 -0800

    utils/db.js

--- utils/db.js
+++ utils/db.js
@@ -1,45 +1,44 @@
-import { MongoClient } from 'mongodb'
-
+import { MongoClient } from 'mongodb';
 
 class DBClient {
   constructor() {
-    const host = process.env.DB_HOST || 'localhost'
-    const port = process.env.DB_PORT || 27017
-    const db = process.env.DB_DATABASE || 'files_manager'
-    
-    const url = `mongodb://${host}:${port}/${db}`
-    this.client = new MongoClient(url, { useUnifiedTopology: true })
-    this.client.connect()
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const db = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}/${db}`;
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
     this.client.on('error', (err) => {
-    console.log(err)
-    })
+      console.log(err);
+    });
   }
 
   isAlive() {
-    return this.client.isConnected()
+    return this.client.isConnected();
   }
 
   async nbUsers() {
-    return this.client.db().collection('users').countDocuments()
+    return this.client.db().collection('users').countDocuments();
   }
 
   async nbFiles() {
-    return this.client.db().collection('files').countDocuments()
+    return this.client.db().collection('files').countDocuments();
   }
 
   async getUsers() {
-    return await this.client.db().collection('user').find({}).toArray
+    return this.client.db().collection('user').find({}).toArray;
   }
 
   async getUserByEmail(email) {
-    return await this.client.db().collection('user').findOne({'email': email})
+    return this.client.db().collection('user').findOne({ email });
   }
 
   async insertUser(doc) {
-    const info = await this.client.db().collection('user').insertOne(doc)
-    return info
+    const info = await this.client.db().collection('user').insertOne(doc);
+    return info;
   }
 }
 
-const dbClient = new DBClient()
-export default dbClient
+const dbClient = new DBClient();
+export default dbClient;
